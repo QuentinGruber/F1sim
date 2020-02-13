27380582b6a4ffc77955200616367ca3a4f197ca
QuentinGruber <QuentinGruber@gmail.com>
2020-02-13 21:14:13 +0100

    Add new random sys to car_bot

--- Classes/Cars_bot.cpp
+++ Classes/Cars_bot.cpp
@@ -3,29 +3,18 @@
 //
 
 #include "Cars_bot.h"
-#include <iostream>
-#include <random>
+#include "utils.h"
 
 
 void Cars_bot::Generate_speed() {
-    std::random_device rd;
-    std::mt19937 mt(rd());
-    std::uniform_real_distribution<double> dist(1.0, 100.0);
-    int rnd_number = dist(mt);
-    int Speed = 200 + (rnd_number * 123) / 100; // Vitesse minimum de 200 max de 323
+    int Speed = 200 + (utils::rnd_number(0.0, 100.0) * 123) / 100; // Vitesse minimum de 200 max de 323
     speed = Speed;
 
 }
 
 void Cars_bot::Random_pit_stop() {
     penality = 0; // reset it at the beginning
-    std::random_device rd;
-    std::mt19937 mt(rd());
-    std::uniform_real_distribution<double> dist(0.0, 100.0);
-    std::uniform_real_distribution<double> dist2(1.0, 20.0);
-    int rnd_number = dist(mt);
-    int rnd_number2 = dist2(mt);
-    if (rnd_number < 10) { // 10 % chance que le bot s'arrête
-        penality = rnd_number2; // add a random penality between 1 and 20
+    if (utils::rnd_number(0.0, 100.0) < 10) { // 10 % chance que le bot s'arrête
+        penality = utils::rnd_number(0.0, 100.0); // add a random penality between 0 and 20
     }
 }
