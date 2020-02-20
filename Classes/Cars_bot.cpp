d7a0dfb4f9ec2774b2add76a16698eb216bc57ff
QuentinGruber <QuentinGruber@gmail.com>
2020-02-20 17:09:17 +0100

    Add car_bot coms

--- Classes/Cars_bot.cpp
+++ Classes/Cars_bot.cpp
@@ -7,21 +7,22 @@
 
 
 void Cars_bot::Generate_speed() {
-    int Speed = 200 + (utils::rnd_number(0.0, 100.0) * 123) / 100; // Vitesse minimum de 200 max de 323
+    // Minimum average speed = 200 Maximum average speed = 323
+    int Speed = 200 + (utils::rnd_number(0.0, 100.0) * 123) / 100;
     speed = Speed;
 
 }
 
 void Cars_bot::Random_pit_stop() {
-    penality = 0; // reset it at the beginning
-    if (utils::rnd_number(0.0, 100.0) < 10) { // 10 % chance que le bot s'arrête
+    penality = 0; // reset penality at the beginning
+    if (utils::rnd_number(0.0, 100.0) < 10) { // 10 % chance that the bot will stop
         penality = utils::rnd_number(0.0, 100.0); // add a random penality between 0 and 20
     }
 }
 
 void Cars_bot::Random_crash() {
-    if(utils::rnd_number(0.0,1000.0) < 1){
+    if (utils::rnd_number(0.0, 1000.0) < 1) { // 1 in 1000 chance that the bot will crash
         HasCrashed = true;
-        global_time = 40404;
+        global_time = 40404; // set global time to "Crash Time"
     }
 }
