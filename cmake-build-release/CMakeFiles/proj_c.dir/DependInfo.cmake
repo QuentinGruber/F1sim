5e0a90b180a629bd96b21393499bcbad56c94711
QuentinGruber <QuentinGruber@gmail.com>
2020-02-21 13:05:34 +0100

    test build release

---
+++ cmake-build-release/CMakeFiles/proj_c.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/grube/Documents/cpp_project/Classes/Cars.cpp" "C:/Users/grube/Documents/cpp_project/cmake-build-release/CMakeFiles/proj_c.dir/Classes/Cars.cpp.obj"
+  "C:/Users/grube/Documents/cpp_project/Classes/Cars_bot.cpp" "C:/Users/grube/Documents/cpp_project/cmake-build-release/CMakeFiles/proj_c.dir/Classes/Cars_bot.cpp.obj"
+  "C:/Users/grube/Documents/cpp_project/Classes/Circuit.cpp" "C:/Users/grube/Documents/cpp_project/cmake-build-release/CMakeFiles/proj_c.dir/Classes/Circuit.cpp.obj"
+  "C:/Users/grube/Documents/cpp_project/Classes/Race.cpp" "C:/Users/grube/Documents/cpp_project/cmake-build-release/CMakeFiles/proj_c.dir/Classes/Race.cpp.obj"
+  "C:/Users/grube/Documents/cpp_project/Classes/utils.cpp" "C:/Users/grube/Documents/cpp_project/cmake-build-release/CMakeFiles/proj_c.dir/Classes/utils.cpp.obj"
+  "C:/Users/grube/Documents/cpp_project/main.cpp" "C:/Users/grube/Documents/cpp_project/cmake-build-release/CMakeFiles/proj_c.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-release/CMakeFiles/proj_c.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proj_c.dir/Classes/Cars.cpp.obj"
+  "CMakeFiles/proj_c.dir/Classes/Cars_bot.cpp.obj"
+  "CMakeFiles/proj_c.dir/Classes/Circuit.cpp.obj"
+  "CMakeFiles/proj_c.dir/Classes/Race.cpp.obj"
+  "CMakeFiles/proj_c.dir/Classes/utils.cpp.obj"
+  "CMakeFiles/proj_c.dir/main.cpp.obj"
+  "proj_c.exe"
+  "proj_c.exe.manifest"
+  "proj_c.lib"
+  "proj_c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proj_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
