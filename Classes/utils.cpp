d1e023b7b68d2241e73f15c0bae09a619976585e
quentingruber <quentingruber@gmail.com>
2023-06-25 03:12:32 +0200

    61x faster

--- Classes/utils.cpp
+++ Classes/utils.cpp
@@ -2,15 +2,16 @@
 // Created by Quentin on 13/02/2020.
 //
 
-#include <random>
 #include "utils.h"
+#include <random>
 
+std::random_device rd;
+std::mt19937 mt(rd()); /// use Mersenne Twister 19937 generator
 int utils::rnd_number(float min, float max) {
-    /// Generate a random floating number between min and max
-    std::random_device rd;
-    std::mt19937 mt(rd()); /// use Mersenne Twister 19937 generator
-    /// A Mersenne Twister pseudo-random generator of 32-bit numbers with a state size of 19937 bits.
-    std::uniform_real_distribution<double> dist(min, max);
-    int rnd_number = dist(mt);
-    return rnd_number;
+  /// Generate a random floating number between min and max
+  /// A Mersenne Twister pseudo-random generator of 32-bit numbers with a state
+  /// size of 19937 bits.
+  std::uniform_real_distribution<double> dist(min, max);
+  int rnd_number = dist(mt);
+  return rnd_number;
 }
